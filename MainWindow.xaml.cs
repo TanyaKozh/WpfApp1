7806d2fd85532666f19fa05c1a08e2e68238f97d
таня кожевникова <kopibaka@outlook.com>
2019-12-17 21:25:13 +0300

    4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -276,6 +276,28 @@ namespace WpfApp1
             }
         }
 
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (myAL != null)
+            {
+                int m = 0;
+                myAL.Add(myAL[0]);
+                //lbMain.Items.Add(myAL[0]);
+                myAL.Add(myAL[1]);
+                //lbMain.Items.Add(myAL[1]);
+                for (int index = 1; index < myAL.Count - 1; index++)
+                {
+                    if (((int)myAL[index - 1] < (int)myAL[index]) && ((int)myAL[index] < (int)myAL[index + 1]))
+                        m++;
+                    if (((int)myAL[index - 1] > (int)myAL[index]) && ((int)myAL[index] > (int)myAL[index + 1]))
+                        m++;
+                }
+                tBox_countElem.BorderBrush = Brushes.Gray;
+                MessageBox.Show("Колличество элементов массива составляющих упорядоченную последовательность с «соседями» = " + m);
+            }
+            else MessageBox.Show("Массив не сформирован");
+        }
+
         private void Btn_task1_Click(object sender, RoutedEventArgs e)
         {
             try
